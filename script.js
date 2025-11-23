785b0f39b21b9ee60b8aea99592632172f1344b9
nina444-dot <ninadominguez1707@gmail.com>
2025-11-23 15:18:45 +0100

    Création du cookies

--- script.js
+++ script.js
@@ -18,3 +18,30 @@ const boutonTheme = document.getElementById("theme-toggle");
 
 let events = [];
 let favorites = loadFavorites();
+
+// Crée le cookie avec une durée d'un an
+function setCookie(name, value, days = 365) {
+	// Calculer la date d'expiration
+	const date = new Date();
+	date.setTime(date.getTime() + days * 24 * 60 * 60 * 1000); // jours en millisecondes
+
+	// Convertir la date en texte pour le cookie
+	const expires = "expires=" + date.toUTCString();
+
+	// Créer le cookie valable sur tout le site
+	document.cookie = name + "=" + value + ";" + expires + ";path=/";
+}
+
+// Lire un cookie
+function getCookie(name) {
+	const cookies = document.cookie.split(";"); // Séparer tous les cookies
+
+	for (let c of cookies) {
+		c = c.trim(); // enlever les espaces
+		if (c.startsWith(name + "=")) {
+			return c.substring(name.length + 1); // retourner la valeur
+		}
+	}
+
+	return "";
+}
